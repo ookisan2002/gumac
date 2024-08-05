8aa683ade8a0e7abc9b6952ef99924e954aca143
ookisan2002 <dainam826@gmail.com>
2024-08-05 17:41:46 +0700

    bug can not build

--- src/Section/Section3/index.jsx
+++ src/Section/Section3/index.jsx
@@ -35,23 +35,44 @@ export default function Section3() {
           src={"/assest/section3/section3_img3.png"}
           className="w-[20.71913rem] h-[20.55494rem] absolute top-[-2rem] left-0 max-md:hidden"
         />
-        <h3 className="text-[8.53333rem] md:text-[3.5rem] font-semibold leading-[150%] text-[#424242]">Mục tiêu của dự án</h3>
+        <h3 className="text-[8.53333rem] md:text-[3.5rem] font-semibold leading-[150%] text-[#424242]">
+          Mục tiêu của dự án
+        </h3>
         <div className=" flex w-fit space-x-[12.27rem] md:space-x-[3.75rem] mt-[12.27rem] md:mt-[3.12rem] flex-wrap max-md:w-[92.27rem] ">
-          <div data-aos="fade-left" className="max-md:mb-[9.87rem] w-[40rem] md:w-[20.9375rem] flex justify-center items-center h-[40rem] md:h-[20.9375rem] rounded-[20.9375rem] bg-[radial-gradient(50%_50%_at_50%_50%,rgba(166,220,229,0.07)_0%,rgba(166,220,229,0.08)_69.16%,rgba(166,220,229,0.26)_100%)]">
-            <p className="w-[26.13333rem] md:w-[14.3125rem] text-center text-[4.26667rem]  md:text-[2rem] font-normal line-clamp-3 text-ellipsis">Tăng tỉ lệ chuyển đổi</p>
+          <div
+            data-aos="fade-left"
+            className="max-md:mb-[9.87rem] w-[40rem] md:w-[20.9375rem] flex justify-center items-center h-[40rem] md:h-[20.9375rem] rounded-[20.9375rem] bg-[radial-gradient(50%_50%_at_50%_50%,rgba(166,220,229,0.07)_0%,rgba(166,220,229,0.08)_69.16%,rgba(166,220,229,0.26)_100%)]"
+          >
+            <p className="w-[26.13333rem] md:w-[14.3125rem] text-center text-[4.26667rem]  md:text-[2rem] font-normal line-clamp-3 text-ellipsis">
+              Tăng tỉ lệ chuyển đổi
+            </p>
           </div>
-          <div data-aos="fade-left" data-aos-delay="100" className="max-md:!translate-y-[18.13rem] max-md:mb-[9.87rem] w-[40rem] md:w-[20.9375rem] flex justify-center items-center h-[40rem] md:h-[20.9375rem] rounded-[20.9375rem] bg-[radial-gradient(50%_50%_at_50%_50%,rgba(166,220,229,0.07)_0%,rgba(166,220,229,0.08)_69.16%,rgba(166,220,229,0.26)_100%)]">
-            <p className="w-[26.13333rem] md:w-[14.3125rem] text-center text-[4.26667rem]  md:text-[2rem] font-normal line-clamp-3 text-ellipsis">Tăng giá trị trung bình đơn</p>
+          <div
+            data-aos="fade-left"
+            data-aos-delay="100"
+            className="max-md:!translate-y-[18.13rem] max-md:mb-[9.87rem] w-[40rem] md:w-[20.9375rem] flex justify-center items-center h-[40rem] md:h-[20.9375rem] rounded-[20.9375rem] bg-[radial-gradient(50%_50%_at_50%_50%,rgba(166,220,229,0.07)_0%,rgba(166,220,229,0.08)_69.16%,rgba(166,220,229,0.26)_100%)]"
+          >
+            <p className="w-[26.13333rem] md:w-[14.3125rem] text-center text-[4.26667rem]  md:text-[2rem] font-normal line-clamp-3 text-ellipsis">
+              Tăng giá trị trung bình đơn
+            </p>
           </div>
-          <div data-aos="fade-left" data-aos-delay="200" className="max-md:mb-[9.87rem] w-[40rem] md:w-[20.9375rem] flex justify-center items-center h-[40rem] md:h-[20.9375rem] rounded-[20.9375rem] bg-[radial-gradient(50%_50%_at_50%_50%,rgba(166,220,229,0.07)_0%,rgba(166,220,229,0.08)_69.16%,rgba(166,220,229,0.26)_100%)]">
-            <p className="w-[26.13333rem] md:w-[14.3125rem] text-center text-[4.26667rem]  md:text-[2rem] font-normal line-clamp-3 text-ellipsis">Giảm phụ thuộc sàn thương mại điện tử</p>
+          <div
+            data-aos="fade-left"
+            data-aos-delay="200"
+            className="max-md:mb-[9.87rem] w-[40rem] md:w-[20.9375rem] flex justify-center items-center h-[40rem] md:h-[20.9375rem] rounded-[20.9375rem] bg-[radial-gradient(50%_50%_at_50%_50%,rgba(166,220,229,0.07)_0%,rgba(166,220,229,0.08)_69.16%,rgba(166,220,229,0.26)_100%)]"
+          >
+            <p className="w-[26.13333rem] md:w-[14.3125rem] text-center text-[4.26667rem]  md:text-[2rem] font-normal line-clamp-3 text-ellipsis">
+              Giảm phụ thuộc sàn thương mại điện tử
+            </p>
           </div>
         </div>
-        <h3 className="mt-[17.23rem] md:mt-[9.37rem] text-[8.53333rem] md:text-[3.5rem] text-[#424242] leading-[150%] font-semibold">3C Research</h3>
+        <h3 className="mt-[17.23rem] md:mt-[9.37rem] text-[8.53333rem] md:text-[3.5rem] text-[#424242] leading-[150%] font-semibold">
+          3C Research
+        </h3>
         <p className="w-[94.66667rem] md:w-[63.4375rem] text-[3.73333rem] md:text-[1.125rem] text-trinh-Gumac-textden leading-[170%] font-manrope font-normal text-center mt-[4.27rem] md:mt-6">
           Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
-          since the 1500s when an unknown printer took a galley of type and
+          industry. Lorem Ipsum has been the industry&apos;s standard dummy text
+          ever since the 1500s when an unknown printer took a galley of type and
           scrambled it to make a type specimen book. of type and scrambled it to
           make a type specimen book.{" "}
         </p>
